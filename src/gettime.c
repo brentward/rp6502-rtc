2272373ed650a570980c97f5380ed13ba8b1deff
Brent Bettis <brent.bettis@gmail.com>
2023-08-24 15:44:04 -0700

    improving test programs, gettime does basic error checking

--- src/gettime.c
+++ src/gettime.c
@@ -4,28 +4,14 @@
 
 void main()
 {
-    datetime_t *datetime;
-    get_rtc_time(datetime);
-    // switch (datetime->dotw) {
-    //     case 0:
-    //         printf("Sunday ");
-    //     case 1:
-    //         printf("Monday ");
-    //     case 2:
-    //         printf("Tuesday ");
-    //     case 3:
-    //         printf("Wednesday ");
-    //     case 4:
-    //         printf("Thursday ");
-    //     case 5:
-    //         printf("Friday ");cd 
-    //     case 6:
-    //         printf("Saturday ");
-    //     case 7:
-    //         printf("Sunday ");
-    //     default:
-    //         printf("Invalid day of week ");
-    // }
+    uint16_t e;
+    datetime_t datetime;
 
-    printf("%d/%02d/%04d %d:%02d:%02d\n", datetime->month, datetime->day, datetime->year, datetime->hour, datetime->min, datetime->sec);
+    e = get_rtc_time(&datetime);
+    if (e)
+    {
+        printf("error: %d", RIA_ERRNO);
+    } else {
+        printf("%d, %d/%02d/%04d %d:%02d:%02d\n", datetime.dotw, datetime.month, datetime.day, datetime.year, datetime.hour, datetime.min, datetime.sec);
+    }
 }
