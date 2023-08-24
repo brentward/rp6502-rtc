27f26f82d161923350c3b93d1278159ee6d9bb87
Brent Bettis <brent.bettis@gmail.com>
2023-08-24 16:06:59 -0700

    better print for datetime

--- src/gettime.c
+++ src/gettime.c
@@ -12,6 +12,33 @@ void main()
     {
         printf("error: %d", RIA_ERRNO);
     } else {
-        printf("%d, %d/%02d/%04d %d:%02d:%02d\n", datetime.dotw, datetime.month, datetime.day, datetime.year, datetime.hour, datetime.min, datetime.sec);
+        switch (datetime.dotw)
+        {
+        case 0:
+            printf("Sunday, ");
+            break;
+        case 1:
+            printf("Monday, ");
+            break;
+        case 2:
+            printf("Tuesday, ");
+            break;
+        case 3:
+            printf("Wednesday, ");
+            break;
+        case 4:
+            printf("Thursday, ");
+            break;
+        case 5:
+            printf("Friday, ");
+            break;
+        case 6:
+            printf("Saturday, ");
+            break;
+        default:
+            printf("Invalid day of week, ");
+            break;
+        }
+        printf("%d/%02d/%04d %d:%02d:%02d\n", datetime.month, datetime.day, datetime.year, datetime.hour, datetime.min, datetime.sec);
     }
 }
