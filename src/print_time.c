197e2a293fd9d9e8b4b604ca6a874134237bf208
Brent Bettis <brent.bettis@gmail.com>
2023-08-27 00:08:52 -0700

    print and set time examples using ISO c and POSIX functions

--- src/print_time.c
+++ src/print_time.c
@@ -6,28 +6,41 @@
 
 void main()
 {
-    // struct timespec now;
-    // int res;
-    // res = clock_gettime(CLOCK_REALTIME, &now);
-    // if (res == -1)
-    // {
-    //     printf("errno: %d\n", errno);
-    // } else 
-    // {
-    //     printf("tv_sec: %lu", now.tv_sec);
-    // }
-    time_t now;
+    struct timespec now;
+    int res;
     struct tm time_struct;
     char buf[80];
-    time(&now);
-    time_struct = *localtime(&now);
-    strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", &time_struct);
-    printf("%s\n", buf);
+    res = clock_gettime(CLOCK_REALTIME, &now);
+    if (res == -1)
+    {
+        printf("errno: %d\n", errno);
+    } else 
+    {
+        time_struct = *localtime(&(now.tv_sec));
+        strftime(buf, sizeof(buf), "%A, %B %d, %Y %I:%M:%S %p %Z", &time_struct);
+        printf("%s\n", buf);
+    }
+
     // time_t now;
     // struct tm time_struct;
     // char buf[80];
-    // now = rtc();
+    // time(&now);
     // time_struct = *localtime(&now);
     // strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", &time_struct);
     // printf("%s\n", buf);
+
+    // time_t now;
+    // struct tm time_struct;
+    // int res;
+    // char buf[80];
+    // res = rtc(&now);
+    // if (res == -1)
+    // {
+    //     printf("errno: %d\n", errno);
+    // } else
+    // {
+    //     time_struct = *localtime(&now);
+    //     strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", &time_struct);
+    //     printf("%s\n", buf);
+    // }
 }
--- src/time_set.c
+++ src/time_set.c
@@ -9,76 +9,61 @@
 
 void main()
 {
-
-    // datetime_t datetime;
     struct tm current_time;
-    // time_t now;
     struct timespec now;
-
     char str[BUF_SIZE];
 
-    current_time.tm_hour = 18;
-    current_time.tm_sec = 0;
-    current_time.tm_min = 34;
-    current_time.tm_mday = 24;
-    current_time.tm_mon = 8;
-    current_time.tm_year = 71;
+    current_time.tm_hour = -1;
+    current_time.tm_sec = -1;
+    current_time.tm_min = -1;
+    current_time.tm_mday = 0;
+    current_time.tm_mon = -1;
+    current_time.tm_year = -1;
     current_time.tm_isdst = -1;
     current_time.tm_wday = 0;
     current_time.tm_yday = 0;
 
+    while (current_time.tm_mon < 0) {
+        puts("Enter month (1-12): ");
+        getsn(str, BUF_SIZE);
+        current_time.tm_mon = atoi(str) - 1;
+        if (current_time.tm_mon > 11)
+            current_time.tm_mon = -1;
+    }
+    while (!current_time.tm_mday) {
+        puts("Enter day (1-31): ");
+        getsn(str, BUF_SIZE);
+        current_time.tm_mday = atoi(str);
+        if (current_time.tm_mday > 31)
+            current_time.tm_mday = 0;
+    }
+    while (current_time.tm_year < 0) {
+        puts("Enter year: ");
+        getsn(str, BUF_SIZE);
+        current_time.tm_year = atoi(str) - 1900;
+    }
+    while (current_time.tm_hour < 0) {
+        puts("Enter hour (0-23): ");
+        getsn(str, BUF_SIZE);
+        current_time.tm_hour = atoi(str);
+        if (current_time.tm_hour > 23)
+            current_time.tm_hour = -1;
+    }
+    while (current_time.tm_min < 0) {
+        puts("Enter minute (0-59): ");
+        getsn(str, BUF_SIZE);
+        current_time.tm_min = atoi(str);
+        if (current_time.tm_min > 59)
+            current_time.tm_min = -1;
+    }
+    while (current_time.tm_sec < 0) {
+        puts("Enter second (0-59): ");
+        getsn(str, BUF_SIZE);
+        current_time.tm_sec = atoi(str);
+        if (current_time.tm_sec > 59)
+            current_time.tm_sec = -1;
+    }
     now.tv_sec = mktime(&current_time);
     now.tv_nsec = 0;
     clock_settime(CLOCK_REALTIME, &now);
-
-    // while (!datetime.month) {
-    //     puts("Enter month (1-12): ");
-    //     getsn(str, BUF_SIZE);
-    //     datetime.month = atoi(str);
-    //     if (datetime.month > 12)
-    //         datetime.month = 0;
-    // }
-    // while (!datetime.day) {
-    //     puts("Enter day (1-31): ");
-    //     getsn(str, BUF_SIZE);
-    //     datetime.day = atoi(str);
-    //     if (datetime.day > 31)
-    //         datetime.day = 0;
-    // }
-    // while (datetime.year < 0) {
-    //     puts("Enter year (0-4095): ");
-    //     getsn(str, BUF_SIZE);
-    //     datetime.year = atoi(str);
-    //     if (datetime.year > 4095)
-    //         datetime.year = -1;
-    // }
-    // while (datetime.hour < 0) {
-    //     puts("Enter hour (0-23): ");
-    //     getsn(str, BUF_SIZE);
-    //     datetime.hour = atoi(str);
-    //     if (datetime.hour > 23)
-    //         datetime.hour = -1;
-    // }
-    // while (datetime.min < 0) {
-    //     puts("Enter minute (0-59): ");
-    //     getsn(str, BUF_SIZE);
-    //     datetime.min = atoi(str);
-    //     if (datetime.min > 59)
-    //         datetime.min = -1;
-    // }
-    // while (datetime.sec < 0) {
-    //     puts("Enter second (0-59): ");
-    //     getsn(str, BUF_SIZE);
-    //     datetime.sec = atoi(str);
-    //     if (datetime.sec > 59)
-    //         datetime.sec = -1;
-    // }
-    // while (datetime.dotw < 0) {
-    //     puts("Enter day of week (0-6, 0 is Sunday): ");
-    //     getsn(str, BUF_SIZE);
-    //     datetime.dotw = atoi(str);
-    //     if (datetime.dotw > 6)
-    //         datetime.dotw = 0;
-    // }
-    // set_rtc_time(&datetime);
 }
